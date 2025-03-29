64e1a13737e7bc162f03aba8a39716cba612af0d
bravoagustin <bravoagustin149@gmail.com>
2025-03-29 00:03:22 -0300

    style: apply styles to navbar

--- frontend/newsapp/src/app/Components/navbar/navbar.component.ts
+++ frontend/newsapp/src/app/Components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { NewsFormComponent } from '../news-form/news-form.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor (private dialog: MatDialog) {}
 
+  openCreateDialog() {
+    this.dialog.open(NewsFormComponent)
+  }
 }
