4a9127f61be1cb7215587df420a608eda81ec013
bravoagustin <bravoagustin149@gmail.com>
2025-03-29 01:08:43 -0300

    feat: add models for news

---
+++ frontend/newsapp/src/app/models/create-news.model.ts
@@ -0,0 +1,7 @@
+export interface CreateNews {
+    title: string;
+    subtitle: string;
+    body: string;
+    image_url: string;
+    author: string;
+  }
---
+++ frontend/newsapp/src/app/models/news.model.ts
@@ -0,0 +1,9 @@
+export interface News {
+    id: string; 
+    title: string;       
+    subtitle: string;   
+    body: string;        
+    image_url: string;   
+    author: string;      
+    date: Date;          
+  }
