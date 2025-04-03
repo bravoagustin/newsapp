15dd19d34a39aec95b2f3a1952ffdd4819b39837
bravoagustin <bravoagustin149@gmail.com>
2025-04-03 05:36:38 -0300

    feat: the console log text was changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server running on port 5000:  http://localhost:${PORT}/api/news`
+    `Server running:  http://localhost:${PORT}/api/news`
   );
 });
 sequelize
