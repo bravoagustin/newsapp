8d0d1daabd2c5dc77c2dd5b7f1ae2cf8d0079d24
bravoagustin <bravoagustin149@gmail.com>
2025-04-03 08:32:33 -0300

    fix: add url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server running:  http://localhost:${PORT}/api/news`
+    `Server running:  ${PORT}`
   );
 });
 sequelize
