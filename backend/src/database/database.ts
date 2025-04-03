ccaac85e16c0a7263f1a51e492675062801a9fc8
bravoagustin <bravoagustin149@gmail.com>
2025-04-03 10:21:51 -0300

    fix: add config dotenv

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -2,16 +2,30 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
+if (!process.env.DATABASE_PUBLIC_URL) {
+    throw new Error('DATABASE_URL no está definida en .env');
+  }
 
-const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+  const sequelize = new Sequelize(process.env.DATABASE_PUBLIC_URL, {
     dialect: 'postgres',
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
+      ssl: { // Obligatorio para Railway
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-    logging: false,
-});
+    logging: console.log, // Opcional: muestra queries SQL en consola
+  });
+
+async function testConnection() {
+    try {
+      await sequelize.authenticate();
+      console.log('✅ Conexión a PostgreSQL establecida correctamente.');
+    } catch (error) {
+      console.error('❌ Error al conectar a PostgreSQL:', error);
+    }
+  }
+
+  testConnection();
 
 export default sequelize;
