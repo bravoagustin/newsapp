00c6dba09ccb1c87d023edc858667e60582cd2e7
bravoagustin <bravoagustin149@gmail.com>
2025-04-03 06:08:10 -0300

    config: the railway url for PostgreSQL was added

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -3,17 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASS as string,
-    {
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        dialect: 'postgres',
-        logging: false,
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Necesario para Railway
+        },
+    },
+    logging: false,
+});
 
 export default sequelize;
-
