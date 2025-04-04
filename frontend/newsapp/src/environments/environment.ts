15b1c2b8af17ee7ece27f95d638e987be4b1beb4
bravoagustin <bravoagustin149@gmail.com>
2025-04-04 03:43:09 -0300

    config: frontend deploy

--- frontend/newsapp/src/environments/environment.ts
+++ frontend/newsapp/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://newsapp-production-b6cb.up.railway.app', 
+  production: true,
+  apiUrl: 'https://newsapp-production-b6cb.up.railway.app/api', 
 };
