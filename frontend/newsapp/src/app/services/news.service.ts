3fbd5a413febbfa773b8f6193e95e9391eac418b
bravoagustin <bravoagustin149@gmail.com>
2025-04-04 04:04:43 -0300

    config: frontend deploy

--- frontend/newsapp/src/app/services/news.service.ts
+++ frontend/newsapp/src/app/services/news.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CreateNewsDto, News } from '../models/news.model';
 import { CreateNews } from '../models/news.model';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
